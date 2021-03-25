ac28f8acde125e794a6e16b915dda8645779cd1c
Marat <Markus_Churchill@mail.ru>
2021-03-25 19:29:01 +0500

    dobavil progu s glasnymi

---
+++ Glasnye.py
@@ -0,0 +1,5 @@
+result=[]
+theword=input()
+print(type(theword))
+result=list(theword)
+print('a', result.count('a'), ',', 'o', result.count('o'), ',', 'u', result.count('u'),'i', result.count('i'), ',', 'e', result.count('e'), ',', 'y', result.count('y'))
